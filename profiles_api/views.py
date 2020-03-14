d771e80c57603bf171e6a37147e9fcd3cdff9fe4
Yuriy Puris <donovanyuriy@gmail.com>
2020-03-14 12:17:17 +0200

    add http methods

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,38 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+from profiles_api import serializers
+
 class TestApiView(APIView):
+
+  serializer_class = serializers.TestSerializer
 
   def get(self, request, format=None):
     return Response({'message': 'Test message'})
+
+  def post(self, request):
+    serializer = self.serializer_class(data=request.data)
+
+    if serializer.is_valid():
+      name = serializer.validated_data.get('name')
+      message = f'Hello {name}!'
+      return Response({'message': message})
+    else:
+      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def put(self, request, pk=None):
+    return Response({'method': 'PUT'})
+
+  def patch(self, request, pk=None):
+    return Response({'method': 'PATCH'})
+
+  def delete(self, request, pk=None):
+    return Response({'method': 'DELETE'})
+
+    
+
+
+
+
+
